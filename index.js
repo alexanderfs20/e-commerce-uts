04835f4b9e435688521879e5d5ccd5c2cb2ed16b
krokerzz <chandreasjoan@gmail.com>
2026-04-08 15:17:11 +0700

    ENV

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const express = require('express');
+const express = require('express'); 
 const app = express();
 
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
 app.use(express.json());
 
 app.get('/', (req, res) => {
