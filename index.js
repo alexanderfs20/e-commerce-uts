00516435069ee295a6152497cb3951a5f03ce6d3
Herman Sim <paulussuherman1@gmail.com>
2026-04-12 19:11:03 +0700

    fitur : sudah menambahkan 6 endpoint

--- index.js
+++ index.js
@@ -1,20 +1,68 @@
+const Product = require('./product.model');
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
 
+const app = express();
 app.use(express.json());
+
+
 
 app.get('/', (req, res) => {
   res.send('API jalan');
 });
 
-app.listen(3000, () => {
-  console.log('http://localhost:3000');
+
+// GET ALL
+app.get('/products', async (req, res) => {
+  const products = await Product.find();
+  res.json({ data: products });
 });
 
-app.get('/products', (req, res) => {
-  res.json([{ id: 1, name: 'Laptop' }]);
+// SEARCH (HARUS DI ATAS)
+app.get('/products/search', async (req, res) => {
+  const { name } = req.query;
+
+  const products = await Product.find({
+    name: { $regex: name, $options: 'i' }
+  });
+
+  res.json({ data: products });
 });
 
-app.post('/products', (req, res) => {
-  res.json({ message: 'Produk ditambahkan' });
+// GET BY ID
+app.get('/products/:id', async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  res.json({ data: product });
 });
+
+// UPDATE
+app.put('/products/:id', async (req, res) => {
+  const product = await Product.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+  res.json({ data: product });
+});
+
+// DELETE
+app.delete('/products/:id', async (req, res) => {
+  await Product.findByIdAndDelete(req.params.id);
+  res.json({ message: 'Deleted' });
+});
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('MongoDB Connected');
+
+    app.listen(3000, () => {
+      console.log('http://localhost:3000');
+    });
+
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
