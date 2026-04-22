cc30ef297008c76c07fbeb811a0a4dd6bd3399d4
pauluskassapa <paulussuherman1@gmail.com>
2026-04-22 10:37:25 +0700

    fitur katalog

--- index.js
+++ index.js
@@ -1,62 +1,18 @@
-const Product = require('./product.model');
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+const productRoutes = require('./Product/routes/product.route');
+
 const app = express();
 app.use(express.json());
-
-
 
 app.get('/', (req, res) => {
   res.send('API jalan');
 });
 
+app.use('/products', productRoutes);
 
-// GET ALL
-app.get('/products', async (req, res) => {
-  const products = await Product.find();
-  res.json({ data: products });
-});
-
-// BUAT NAMBAHIN DATA
-app.post('/products', async (req, res) => {
-  const product = await Product.create(req.body);
-  res.json({ data: product });
-});
-
-// SEARCH
-app.get('/products/search', async (req, res) => {
-  const { name } = req.query;
-
-  const products = await Product.find({
-    name: { $regex: name, $options: 'i' }
-  });
-
-  res.json({ data: products });
-});
-
-// GET BY ID
-app.get('/products/:id', async (req, res) => {
-  const product = await Product.findById(req.params.id);
-  res.json({ data: product });
-});
-
-// UPDATE
-app.put('/products/:id', async (req, res) => {
-  const product = await Product.findByIdAndUpdate(
-    req.params.id,
-    req.body,
-    { new: true }
-  );
-  res.json({ data: product });
-});
-
-// DELETE
-app.delete('/products/:id', async (req, res) => {
-  await Product.findByIdAndDelete(req.params.id);
-  res.json({ message: 'Deleted' });
-});
 const start = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
